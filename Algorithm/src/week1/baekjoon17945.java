3b9b5a5c17ab9a2a3c00dd5a257f44c4514bb350
hyeyeonnnnn <115079024+hyeyeonnnnn@users.noreply.github.com>
2023-07-04 17:11:41 +0900

    [ADD] 백준 17945

--- Algorithm/src/week1/baekjoon17945.java
+++ Algorithm/src/week1/baekjoon17945.java
@@ -1,5 +1,33 @@
 package week1;
 
-public class baekjoon17945 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class baekjoon17945 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int root = (int)Math.sqrt(4*a*a-4*b);
+    	int result1 = (-2*a-root)/2;
+    	int result2 = (-2*a+root)/2;
+    	
+       if(result1==result2) {
+    	   sb.append(result1);
+       }
+       else if(result1<result2) {
+    	   sb.append(result1 + " " + result2);
+       }
+       else {
+    	   sb.append(result2 + " " + result1);
+       }   	   
+    	   
+        System.out.println(sb);
+	}
 }
